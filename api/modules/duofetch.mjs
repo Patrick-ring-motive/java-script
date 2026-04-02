e37587b6a224f0702c97721bb1a23246e2b941f6
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 17:51:45 -0500

    style: js-beautify formatting (api/modules/duofetch.mjs)

--- api/modules/duofetch.mjs
+++ api/modules/duofetch.mjs
@@ -1,19 +1,17 @@
-import nodeFetch  from 'node-fetch';
+import nodeFetch from 'node-fetch';
 
-
-globalThis.duofetch = function(){
-const flip=Math.floor(Math.random() *2);
-let primeFetch=fetch;
-let secFetch=nodeFetch;
-  if(flip){
-    primeFetch=nodeFetch;
-    secFetch=fetch;
+globalThis.duofetch = function() {
+  const flip = Math.floor(Math.random() * 2);
+  let primeFetch = fetch;
+  let secFetch = nodeFetch;
+  if (flip) {
+    primeFetch = nodeFetch;
+    secFetch = fetch;
   }
-  try{
+  try {
     return primeFetch(...arguments);
-  }catch(e){
+  } catch (e) {
     return secFetch(...arguments);
   }
-
 
 };
